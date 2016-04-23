4c74226001854117e020e8e1e131d1818ccae8e5
rakeshds <rakeshds@users.noreply.github.com>
2016-04-23 23:07:30 +0530

    Added files via upload

---
+++ run_analysis.R
@@ -0,0 +1,32 @@
+##load package
+library(resahpe2)
+library(dplyr)
+##downlaoding of data
+url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+f<-file.path(getwd(),"gdpf.zip")
+download.file(url,f)
+##data make up
+actname<-read.table("./UCIHARDataset/activity_labels.txt")
+name_feature<-read.table("./UCIHARDataset/features.txt")
+name_feature[,2] <- as.character(name_feature[,2])
+extract_feature<-grep(".*mean.*|.*std.*",name_feature[,2])
+extract_feature.names<-name_feature[extract_feature,2]
+
+##training
+train_x<- read.table("./UCIHARDataset/train/X_train.txt")[extract_feature]
+train_y<-read.table("./UCIHARDataset/train/y_train.txt")
+train_sub<-read.table("./UCIHARDataset/train/subject_train.txt")
+train<-cbind(train_x,train_y,train_sub)
+##testing
+test_x<- read.table("./UCIHARDataset/test/X_test.txt")[extract_feature]
+test_y<-read.table("./UCIHARDataset/test/y_test.txt")
+test_sub<-read.table("./UCIHARDataset/test/subject_test.txt")
+test<-cbind(test_x,test_y,test_sub)
+
+##merge data
+merge_data<-rbind(test,train)
+colnames(merge_data)<-c(extract_feature.names,"activity","subject")
+
+##tidy data
+average.df <- aggregate(x=merge_data, by=list(act=merge_data$activity, subj=merge_data$subject), FUN=mean)
+write.table(average.df, './UCIHARDataset/average.txt', row.names = F)
